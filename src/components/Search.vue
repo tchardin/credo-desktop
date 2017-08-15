1cd503908e6cfaf71f90f8a7b5592f867cf24d2a
tdot <thomaschardin@hotmail.fr>
2017-08-15 17:48:28 -0400

    first commit

---
+++ src/components/Search.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'search'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .input {
+
+  }
+</style>
